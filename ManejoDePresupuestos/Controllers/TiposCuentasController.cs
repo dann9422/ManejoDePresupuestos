f92df8d8509c66e5cbd3a9fcd4f0a33ca9723ca5
dann9422 <dannsk82294@gmail.com>
2023-05-10 18:08:10 -0600

    Crud de tipoCuentas

--- ManejoDePresupuestos/Controllers/TiposCuentasController.cs
+++ ManejoDePresupuestos/Controllers/TiposCuentasController.cs
@@ -7,18 +7,20 @@ namespace ManejoDePresupuestos.Controllers
 {
     public class TiposCuentasController : Controller
     {
-        private readonly IRepositorioTiposCuentas repositoriTipoCuentas;
+        private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
+        private readonly IservicioUsuarios servicioUsuarios;
 
-        public TiposCuentasController(IRepositorioTiposCuentas repositoriTipoCuentas)
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas,
+            IservicioUsuarios servicioUsuarios)
         {
-            this.repositoriTipoCuentas = repositoriTipoCuentas;
+            this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
         }
-
 
         public async Task<IActionResult> Index()
         {
-            var usuarioId = 1;
-            var tiposCuentas = await repositoriTipoCuentas.Obtener(usuarioId);
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
             return View(tiposCuentas);
         }
 
@@ -28,44 +30,122 @@ namespace ManejoDePresupuestos.Controllers
         }
 
         [HttpPost]
-        public async Task <IActionResult>Crear(TipoCuenta tipoCuenta)
-
-         {
+        public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
+        {
             if (!ModelState.IsValid)
             {
                 return View(tipoCuenta);
             }
-            tipoCuenta.UsuarioId = 1;
 
-            var yaExisteTipoCuenta = await repositoriTipoCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            var yaExisteTipoCuenta =
+                await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
 
             if (yaExisteTipoCuenta)
             {
-                ModelState.AddModelError(nameof(tipoCuenta.Nombre), $"El nombre{tipoCuenta.Nombre} ya existe");
+                ModelState.AddModelError(nameof(tipoCuenta.Nombre),
+                    $"El nombre {tipoCuenta.Nombre} ya existe.");
 
                 return View(tipoCuenta);
-
             }
 
-            await repositoriTipoCuentas.Crear(tipoCuenta);
+            await repositorioTiposCuentas.Crear(tipoCuenta);
 
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Editar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Editar(TipoCuenta tipoCuenta)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuentaExiste = await repositorioTiposCuentas.ObtenerPorId(tipoCuenta.Id, usuarioId);
+
+            if (tipoCuentaExiste is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Actualizar(tipoCuenta);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Borrar(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            return View(tipoCuenta);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrarTipoCuenta(int id)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tipoCuenta = await repositorioTiposCuentas.ObtenerPorId(id, usuarioId);
+
+            if (tipoCuenta is null)
+            {
+                return RedirectToAction("NoEncontrado", "Home");
+            }
+
+            await repositorioTiposCuentas.Borrar(id);
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
         {
-            var usuarioId = 1;
-            var yaExisteTipoCuenta = await repositoriTipoCuentas.Existe(nombre, usuarioId);
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(nombre, usuarioId);
 
             if (yaExisteTipoCuenta)
             {
-                return Json($"el nombre {nombre} ya existe");
+                return Json($"El nombre {nombre} ya existe");
             }
+
             return Json(true);
-        } 
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            var usuarioId = servicioUsuarios.ObtenerUsuarioId();
+            var tiposCuentas = await repositorioTiposCuentas.Obtener(usuarioId);
+            var idsTiposCuentas = tiposCuentas.Select(x => x.Id);
 
+            var idsTiposCuentasNoPertenecenAlUsuario = ids.Except(idsTiposCuentas).ToList();
 
+            if (idsTiposCuentasNoPertenecenAlUsuario.Count > 0)
+            {
+                return Forbid();
+            }
+
+            var tiposCuentasOrdenados = ids.Select((valor, indice) =>
+                new TipoCuenta() { Id = valor, Orden = indice + 1 }).AsEnumerable();
+
+            await repositorioTiposCuentas.Ordenar(tiposCuentasOrdenados);
+
+            return Ok();
+        }
     }
 }
---
+++ ManejoDePresupuestos/Servicios/ServicioUsuarios.cs
@@ -0,0 +1,18 @@
+﻿namespace ManejoDePresupuestos.Servicios
+{
+
+    public interface IservicioUsuarios
+    {
+        int ObtenerUsuarioId();
+    }
+
+
+    public class ServicioUsuarios : IservicioUsuarios
+    {
+
+        public int ObtenerUsuarioId()
+        {
+            return 1;   
+        }
+    }
+}
